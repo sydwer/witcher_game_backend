4819a8c0d6c5a3fec39db2c57fab767c5ab53643
Sydney Werbach <sydwer@gmail.com>
2020-01-02 16:17:40 -0700

    switched image for big bad wold seed

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,11 +1,11 @@
 class MonstersController < ApplicationController
     def index
         monsters = Monster.all 
-        render json: monsters
+        render json: monsters, include: [:oil, :sign, :place, :time_of_day]
     end
 
     def show
         monster = Monster.find(params[:id])
-        render json: monster
+        render json: monster, include: [:oil, :sign, :place, :time_of_day]
     end
 end
